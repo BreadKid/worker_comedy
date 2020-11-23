737527702541e3241f2d39ca85c18b78872db3ed
BreadKid <85332693@qq.com>
2020-11-23 16:01:50 +0800

    test

---
+++ daily_life/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @description:
+ * @author: Joyce Liu
+ * @create: 2020-11-23 16:00
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
